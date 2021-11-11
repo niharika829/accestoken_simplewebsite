a91fd268a712feb05305289f78b1d7d075da389a
Niharika Sharma <ns1324y@gmail.com>
2021-11-11 16:10:34 +0530

    added get api to fetch list of post

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // to import express
 const express = require("express")
+const postData = require("./data/posts")
 
 // we need to have a app variable to setput the express server
 const app = express()
+
+
+app.get('/posts', (req, res) => {
+    res.json({ message: "success", data: postData.postArr })
+})
 
 // the app variable will therfore listen to the specified port
 app.listen(5000)
